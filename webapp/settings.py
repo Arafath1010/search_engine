eebd0084d5979d53be53a8290187ecd322be0281
Arafath1010 <107343055+Arafath1010@users.noreply.github.com>
2023-01-31 22:34:19 +0530

    Update settings.py

--- webapp/settings.py
+++ webapp/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'use ur key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -129,6 +129,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 
 ]
+STATIC_ROOT = os.path.join( BASE_DIR,'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
