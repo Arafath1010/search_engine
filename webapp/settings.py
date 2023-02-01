48df94ad51f1de16b1886dcedc6b82018702ca97
Arafath1010 <107343055+Arafath1010@users.noreply.github.com>
2023-02-01 10:30:54 +0530

    Update settings.py

--- webapp/settings.py
+++ webapp/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'use ur key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['searchengine-production.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,12 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
+STATIC_ROOT = os.path.join( BASE_DIR,'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join( BASE_DIR,"static"),
 ]
 #STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_ROOT = os.path.join( BASE_DIR,'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
